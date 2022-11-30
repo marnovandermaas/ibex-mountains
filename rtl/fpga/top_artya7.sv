c0be0a42982e405ddbab8d9796086595e14edfe2
Marno van der Maas <mvdmaas+git@lowrisc.org>
2022-11-30 11:07:05 +0000

    Temporarily add switches and buttons to test assignment to LEDs

--- rtl/fpga/top_artya7.sv
+++ rtl/fpga/top_artya7.sv
@@ -7,13 +7,18 @@ module top_artya7 (
   // These inputs are defined in data/pins_artya7.xdc
   input               IO_CLK,
   input               IO_RST_N,
-  output [3:0]        LED,
-  output [11:0]       RGB_LED,
+  input         [3:0] SW,
+  input         [3:0] BTN,
+  output logic  [3:0] LED,
+  output logic [11:0] RGB_LED,
   output              UART_TX
 );
-  parameter              SRAMInitFile = "";
+  parameter SRAMInitFile = "";
 
   logic clk_sys, rst_sys_n;
+
+  logic  [3:0] ibex_led;
+  logic [11:0] ibex_rgb_led;
 
   // Instantiating the Ibex Demo System.
   ibex_demo_system #(
@@ -23,9 +28,17 @@ module top_artya7 (
     .clk_sys_i(clk_sys),
     .rst_sys_ni(rst_sys_n),
 
-    .gp_o({LED, RGB_LED}),
+    .gp_o({ibex_led, ibex_rgb_led}),
     .uart_tx_o(UART_TX)
   );
+
+  always_ff @(posedge clk_sys) begin
+    LED[0] <= BTN[0];
+    LED[1] <= BTN[1] ? 1'b0 : ibex_led[1];
+    LED[2] <= BTN[2] ? 1'b0 : ibex_led[2];
+    LED[3] <=  SW[3];
+    RGB_LED <= ibex_rgb_led;
+  end
 
   // Generating the system clock and reset for the FPGA.
   clkgen_xil7series clkgen(
