8473d9a29c435e14570be75820bfd058777c6b02
Marno van der Maas <mvdmaas+git@lowrisc.org>
2022-12-06 15:40:31 +0000

    Add copyright header to PWM

--- rtl/system/pwm.sv
+++ rtl/system/pwm.sv
@@ -1,3 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 module pwm #(
   parameter int CtrSize = 8,
   parameter int IOLength = 1
