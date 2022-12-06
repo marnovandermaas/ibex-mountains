f841ebfe52233692d0799e0f581a44d1666f264b
Marno van der Maas <mvdmaas+git@lowrisc.org>
2022-12-06 16:37:05 +0000

    Removed unmodulate passthrough and added max counter input for PWM

--- rtl/system/pwm.sv
+++ rtl/system/pwm.sv
@@ -3,29 +3,33 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module pwm #(
-  parameter int CtrSize = 8,
-  parameter int IOLength = 1
+  parameter int CtrSize = 8
 ) (
-  input logic                 clk_sys_i,
-  input logic                 rst_sys_ni,
+  input  logic               clk_sys_i,
+  input  logic               rst_sys_ni,
 
-  input  logic [CtrSize-1:0]  pulse_width_i,
-  input  logic [IOLength-1:0] unmodulated_i,
+  // If you want an always on signal, you will need to make pulse_width_i > max_counter_i.
+  input  logic [CtrSize-1:0] pulse_width_i,
+  input  logic [CtrSize-1:0] max_counter_i,
 
-  output logic [IOLength-1:0] modulated_o
+  output logic               modulated_o
 );
   logic [CtrSize-1:0] counter;
 
   always_ff @(posedge clk_sys_i) begin
-    if (!rst_sys_ni) begin
+    if (!rst_sys_ni || max_counter_i == 0) begin
       counter <= 'b0;
       modulated_o <= 'b0;
     end else begin
-      counter <= counter + 1;
-      if (pulse_width_i >= counter) begin
-        modulated_o <= unmodulated_i;
+      if (counter < max_counter_i) begin
+        counter <= counter + 1;
       end else begin
-        modulated_o <= 'b0;
+        counter <= 0;
+      end
+      if (pulse_width_i > counter) begin
+        modulated_o <= 1'b1;
+      end else begin
+        modulated_o <= 1'b0;
       end
     end
   end
