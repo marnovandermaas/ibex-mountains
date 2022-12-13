57c8805848cd19374bd17731a0d6715f4b24a618
Marno van der Maas <mvdmaas+git@lowrisc.org>
2022-12-13 16:09:15 +0000

    Add comments to pwm.sv

--- rtl/system/pwm.sv
+++ rtl/system/pwm.sv
@@ -8,7 +8,7 @@ module pwm #(
   input  logic               clk_i,
   input  logic               rst_ni,
 
-  // If you want an always on signal, you will need to make pulse_width_i > max_counter_i.
+  // To produce an always-on signal, you will need to make pulse_width_i > max_counter_i.
   input  logic [CtrSize-1:0] pulse_width_i,
   input  logic [CtrSize-1:0] max_counter_i,
 
@@ -21,14 +21,17 @@ module pwm #(
       counter     <= 'b0;
       modulated_o <= 'b0;
     end else if (max_counter_i == 0) begin
+      // Set output to low when maximum is zero.
       counter     <= 'b0;
       modulated_o <= 'b0;
     end else begin
+      // Wrap the counter once it gets to the maximum.
       if (counter < max_counter_i) begin
         counter <= counter + 1;
       end else begin
         counter <= 0;
       end
+      // Set output to high for pulse_width_i/max_counter_i amount of time.
       if (pulse_width_i > counter) begin
         modulated_o <= 1'b1;
       end else begin
